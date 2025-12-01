d64bdaca22f630b6c853ae20f1bd6d6844e7721c
Wisdom Mpamugo <wisdommpam99@gmail.com>
2025-12-01 14:53:25 +0100

    second commit

---
+++ frontend/src/lib/products.js
@@ -0,0 +1,90 @@
+export const newArrivals = [
+  {
+    _id: "1",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=1",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "2",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=2",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "3",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=3",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "4",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=4",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "5",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=5",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "6",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=6",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "7",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=7",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+  {
+    _id: "8",
+    name: "Stylish Jacket",
+    price: 15000,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=8",
+        altText: "Stylish Jacket",
+      },
+    ],
+  },
+];
